aa7aa3b2cc4bb7093dfa02393d5073e32b141563
varunsureshkumar-77 <varunsureshkumar77@gmail.com>
2025-12-07 17:10:00 -0500

    feat: frontend url change

--- frontend/src/config/api.config.js
+++ frontend/src/config/api.config.js
@@ -1,7 +1,7 @@
 // API Configuration
 const config = {
   apiUrl: import.meta.env.PROD 
-    ? 'https://v55-backend-5ab44bfcd040.herokuapp.com/api'
+    ? 'http://159.65.173.35/api'
     : 'https://localhost:5000/api',
 };
 
