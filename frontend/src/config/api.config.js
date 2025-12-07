125a81a7218f171bfd5b0734941f3a22432ce06b
varunsureshkumar-77 <varunsureshkumar77@gmail.com>
2025-12-07 17:41:46 -0500

    chore: change in url

--- frontend/src/config/api.config.js
+++ frontend/src/config/api.config.js
@@ -1,7 +1,7 @@
 // API Configuration
 const config = {
   apiUrl: import.meta.env.PROD 
-    ? 'https://159.65.173.35/api'
+    ? 'https://159.65.173.35.nip.io/api'
     : 'https://localhost:5000/api',
 };
 
